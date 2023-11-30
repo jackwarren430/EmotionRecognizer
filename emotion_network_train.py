753f964be65c29eb1b83ca6e67815456e6afed74
Jack <jackwarren430@gmail.com>
2023-11-29 22:12:35 -0500

    fixed up parallel processing

--- emotion_network_train.py
+++ emotion_network_train.py
@@ -1,5 +1,6 @@
 from multiprocessing.pool import ThreadPool as Pool
 import numpy as np
+from functools import partial
 
 x_train = np.load("data/raw/x_train.npy")
 y_train = np.load("data/raw/y_train.npy")
@@ -25,6 +26,7 @@ def softmax(Z):
     return probabilities
 
 def forwardProp(W1r, W1h, B1, W2, B2, X):
+    #fix
     outputs = np.zeros((216, 8))
     hidden_states = np.zeroes((216, 10))
     hidden_layer = np.zeros((10, 1))
@@ -35,26 +37,24 @@ def forwardProp(W1r, W1h, B1, W2, B2, X):
     A2 = softmax(Z2)
     return hidden_states, Z2, A2
 
-def one_hot_Y(y_train):
-    one_hot_Y = np.zeros((8, 300))
-    for i in range(300):
-        index = int(y_train[i]) - 1
-        one_hot_Y[index][i] = 1
-    return one_hot_Y
-
-def backwardProp(hidden_states, Z2, A2, W2, Y):
-    Y = one_hot_Y(Y)
+def backwardProp(hidden_states, Z2, A2, W2, y):
+    #fix
+    Y = np.zeros((8,1))
+    Y[y][0] = 1
     dW1r, dW1h, dW2 = 0, 0, 0
     dB1, dB2 = 0, 0          
     dhidden_next = 0 
-    dZ2 = A2 - Y
-    for i in reversed(range(216)):
-        dW2_t = dZ2.dot(hidden_states[i].T)
+    gradient = A2 - Y
+    for t in reversed(range(216)):
+        
+
         dB2_t = 
         dhidden = W2.T.dot(dZ2) * relu_prime(hidden_states[i])
         dW1r_t = dhidden @ sequence[t].T
         dW1h_t = dhidden @ hidden_states[t-1].T if t > 0 else 0
         dB1_t = 
+
+
         dW1r += dW1r_t
         dW1h += dW1h_t
         dB1 += dB1_t
@@ -63,35 +63,35 @@ def backwardProp(hidden_states, Z2, A2, W2, Y):
 
     return dW1r, dW1h, dB1, dW2, dB2
 
-def processSequence(X):
-    global batch_dW1r, batch_dW1h, batch_dW2, batch_dB1, batch_dB2
+def updateParams(W1r, W1h, B1, W2, B2, results, batch_size, alpha):
+    W1r -= alpha * np.sum(results[:,0]) / batch_size
+    W1h -= alpha * np.sum(results[:,1]) / batch_size
+    B1 -= alpha * np.sum(results[:,2]) / batch_size
+    W2 -= alpha * np.sum(results[:,3]) / batch_size
+    B2 -= alpha * np.sum(results[:,4]) / batch_size
+    return W1r, W1h, B1, W2, B2
 
-    hidden_states, Z2, A2 = forwardProp()
-    dW1r, dW1h, dB1, dW2, dB2 = backwardProp()
-
-    batch_dW1r += dW1r
-    batch_dW1h += dW1h
-    batch_dB1 += dB1
-    batch_dW2 += dW2
-    batch_dB2 += dB2
+def processSequence(batch, weights_biases):
+    x, y = batch
+    W1r, W1h, B1, W2, B2 = weights_biases
+    hidden_states, Z2, A2 = forwardProp(W1r, W1h, B1, W2, B2, x_batch)
+    dW1r, dW1h, dB1, dW2, dB2 = backwardProp(hidden_states, Z2, A2)
+    return [dW1r, dW1h, dB1, dW2, dB2]
 
 def gradient_descent(X, Y, iterations, alpha):
     W1r, W1h, B1, W2, B2 = initParams()
-
+    batch_size = 30
     for i in range(iterations):
-        batch_size = 30
-        for j in range(10):
+        for j in range(300/batch_size):
             batch_dW1r, batch_dW1h, batch_dW2 = 0, 0, 0
             batch_dB1, batch_dB2 = 0, 0
-            batch = X[j*30 : (j*30)+30]
-            with Pool(processes=30) as pool:
-                pool.map(processSequence, batch)
-
-            W1r -= alpha * batch_dW1r / batch_size
-            W1h -= alpha * batch_dW1h / batch_size
-            B1 -= alpha * batch_dB1 / batch_size
-            W2 -= alpha * batch_dW2 / batch_size
-            B2 -= alpha * batch_dB2 / batch_size
+            x_batch = X[j*batch_size : (j*batch_size)+batch_size]
+            y_batch = Y[j*batch_size : (j*batch_size)+batch_size]
+            weights_biases = [W1r, W1h, B1, W2, B2]
+            partial_function = partial(processSequence, weights_biases=weights_biases)
+            with Pool(processes=batch_size) as pool:
+                results = pool.map(partial_function, zip(x_batch, y_batch))
+            W1r, W1h, B1, W2, B2 = updateParams(W1r, W1h, B1, W2, B2, results, batch_size, alpha)
     return W1r, W1h, B1, W2, B2
 
 
