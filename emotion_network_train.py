5e8749781bc30b54629482f3ffad88493a0c5675
jackwarren430 <90930942+jackwarren430@users.noreply.github.com>
2023-12-17 00:58:00 -0500

    major overhaul: added gru

--- emotion_network_train.py
+++ emotion_network_train.py
@@ -5,23 +5,38 @@ from functools import partial
 x_train = np.load("data/raw/x_train.npy")
 y_train = np.load("data/raw/y_train.npy")
 
+mean = np.mean(x_train, axis=(0, 1))
+std_dev = np.std(x_train, axis=(0, 1))
+std_dev[std_dev == 0] = 1
+x_train = (x_train - mean) / std_dev
 
 amount_correct = 0
 amount_tested = 0
 
 def initParams():
-    Wx = np.random.randn(10, 13) * np.sqrt(2. / 13)
-    Wh = np.random.randn(10, 10) * np.sqrt(2. / 10)
-    B1 = np.zeros((10, 1))
-    Wo = np.random.randn(8, 10) * np.sqrt(2. / 10)
-    B2 = np.zeros((8, 1))
-    return Wx, Wh, B1, Wo, B2
+    xl = np.sqrt(6 / (10 + 13))
+    hl = np.sqrt(6 / (10 + 10))
+    ol = np.sqrt(6 / (8 + 10))
+    np.random.uniform(-xl, xl, size=(10, 13))
+    Wxz = np.random.uniform(-xl, xl, size=(10, 13))
+    Whz = np.random.uniform(-hl, hl, size=(10, 10))
+    Bz = np.zeros((10, 1))
+    Wxr = np.random.uniform(-xl, xl, size=(10, 13))
+    Whr = np.random.uniform(-hl, hl, size=(10, 10))
+    Br = np.zeros((10, 1))
+    Wxh = np.random.uniform(-xl, xl, size=(10, 13))
+    Whh = np.random.uniform(-hl, hl, size=(10, 10))
+    Bh = np.zeros((10, 1))
+    Wo = np.random.uniform(-ol, ol, size=(8, 10))
+    Bo = np.zeros((8, 1))
+    return Wxz, Whz, Bz, Wxr, Whr, Br, Wxh, Whh, Bh, Wo, Bo
 
-def relu(Z):
-    return np.maximum(0, Z)
+def sigmoid(v):
+    return 1 / (1 + np.exp(-v))
 
-def relu_prime(Z):
-    return Z > 0
+def sigmoid_prime(v):
+    sv = sigmoid(v)
+    return sv * (1 - sv)
 
 def tanh(Z):
     return np.tanh(Z)
@@ -33,73 +48,81 @@ def softmax(Z):
     e_Z = np.exp(Z - np.max(Z))
     return e_Z / np.sum(e_Z, 0)
 
-def forwardProp(Wx, Wh, B1, Wo, B2, X):
-    hidden_states = np.zeros((216, 10))
-    hidden_states_u = np.zeros((216, 10))
-    hidden_layer = np.zeros((10, 1))
-    for i in range(216):
-        hidden_layer = Wx.dot(X[i]) + Wh.dot(hidden_layer).reshape(-1) + B1.reshape(-1)
-        hidden_layer = 2*((hidden_layer - np.min(hidden_layer)) / (np.max(hidden_layer) - np.min(hidden_layer))) - 1
-        hidden_states_u[i] = hidden_layer
-        hidden_states[i] = relu(hidden_layer)
-    Z = Wo.dot(relu(hidden_layer)).reshape((8, 1)) + B2.reshape((8, 1))
-    #print("^^^^^^\nZ:\n{}\n^^^^^^^^".format(Z))
-    A = softmax(Z)
-    #print("^^^^^^\nA:\n{}\n^^^^^^^^".format(A))
-    return hidden_states_u, hidden_states, Z, A
+def forwardProp(weights_biases, X):
+    Wxz, Whz, Bz, Wxr, Whr, Br, Wxh, Whh, Bh, Wo, Bo = weights_biases
+    Uz_layers = np.zeros((216, 10))
+    Z_layers = np.zeros((216, 10))
+    Ur_layers = np.zeros((216, 10))
+    R_layers = np.zeros((216, 10))
+    Uh_layers = np.zeros((216, 10))
+    Hhat_layers = np.zeros((216, 10))
+    H_layers = np.zeros((216, 10))
+    H = np.zeros((10, 1))
+    for t in range(216):
+        Uz = Wxz @ X[t] + Whz @ H + Bz
+        Z = sigmoid(Uz)
+        Ur = Wxr @ X[t] + Whr @ H + Br
+        R = sigmoid(Ur)
+        Uh = Wxh @ X[t] + Whh @ (R * H) + Bh
+        Hhat = tanh(Uh)
+        H = (1 - Z) * H + Z * Hhat
+        Uz_layers[t] = Uz
+        Z_layers[t] = Z
+        Ur_layers[t] = Ur
+        R_layers[t] = R
+        Uh_layers[t] = Uh
+        Hhat_layers[t] = Hhat 
+        H_layers[t] = H
+    Z2 = Wo @ H + Bo
+    A = softmax(Z2)
+    return Uz_layers, Z_layers, Ur_layers, R_layers, Uh_layers, Hhat_layers, Hhat_layers, Z2, A
 
-def backwardProp(hidden_states_u, hidden_states, Z, A, Wh, Wo, y, X):
+def calculateGradients(foward_results, weights_biases, t, X):
+    Uz_layers, Z_layers, Ur_layers, R_layers, Uh_layers, Hhat_layers, Hhat_layers, Z2, A = foward_results
+    Wxz, Whz, Bz, Wxr, Whr, Br, Wxh, Whh, Bh, Wo, Bo = weights_biases
+    if (t == 0):
+        #base case
+        return
+    dh_Wxz, dh_Whz, dh_Bz, dh_Wxr, dh_Whr, dh_Br, dh_Wxh, dh_Whh, dh_Bh, dh_Wo, dh_Bo = calculateGradients(foward_results, weights_biases, t)
+    dh_Wxz = (sigmoid_prime(Uz_layers[t]) @ (X[t] + Whz @ ))
+    dh_Whz
+    dh_Bz
+    dh_Wxr
+    dh_Whr
+    dh_Br
+    dh_Wxh
+    dh_Whh
+    dh_Bh
+    dh_Wo
+    dh_Bo
+    return dh_Wxz, dh_Whz, dh_Bz, dh_Wxr, dh_Whr, dh_Br, dh_Wxh, dh_Whh, dh_Bh, dh_Wo, dh_Bo
+
+def backwardProp(foward_results, weights_biases, y, X):
     Y = np.zeros((8,1))
     Y[int(y-1)][0] = 1
-    dWx = np.zeros((10, 13))
-    dWh = np.zeros((10, 10))
-    dB1 = np.zeros((10, 1))
     
-    gradient = A - Y
-    dB2 = gradient / 13
-    dWo = gradient @ hidden_states[-1].reshape((1, 10))
-    agg = 1
-    for t in reversed(range(216)):
-        curr_x = X[t]
-        curr_hid_u = hidden_states_u[t]
-        prev_hid = hidden_states[t - 1] if t >= 0 else 0
+    t = 216
+    dh_Wxz, dh_Whz, dh_Bz, dh_Wxr, dh_Whr, dh_Br, dh_Wxh, dh_Whh, dh_Bh, dh_Wo, dh_Bo = calculateGradients(foward_results, weights_biases, t, X)
+    dWxz
+    dWhz
+    dBz
+    dWxr
+    dWhr
+    dBr
+    dWxh
+    dWhh
+    dBh
+    dWo
+    dBo
 
-        if (t == 215):
-            #print("\n\n\n---\nWo.T:\n{}\n\ncurr_hid_u:\n{}\n---\n\n".format(Wo.T, curr_hid_u))
-            agg = (Wo.T @ gradient) * relu_prime(curr_hid_u).reshape((10, 1))
-            #print("\n\n\n---\nagg:\n{}\n\ncurr X:\n{}\n---\n\n*******".format(agg, curr_x.T))
-        else:
-            agg = (Wh @ agg) * relu_prime(curr_hid_u).reshape((10, 1)) 
-            #print("\n\ncurr_hid_u:\n{}\n---\n\n".format(curr_hid_u))
-
-        #look into subtracting a value before multiplying, then add them back in
-        dWx_t = np.outer(agg, curr_x.T)
-        dWh_t = np.outer(agg, prev_hid)
-        dB1_t = agg / 13
-
-        dWx += dWx_t
-        dWh += dWh_t
-        dB1 += dB1_t
-    
-    hold = "\n\n\n----------\ndWx:\n{}\n---\ndWh:\n{}\n---\ndB1:\n{}\n\n".format(dWx, dWh, dB1)
-
+   
+    '''
     Wx_threshold = 5
     dWx_norm = np.linalg.norm(dWx, ord=2)
     if dWx_norm > Wx_threshold:
         dWx *= Wx_threshold / dWx_norm
-    Wh_threshold = 5
-    dWh_norm = np.linalg.norm(dWh, ord=2)
-    if dWh_norm > Wh_threshold:
-        dWh *= Wh_threshold / dWh_norm
-    B_threshold = 5
-    dB1_norm = np.linalg.norm(dB1, ord=2)
-    if dB1_norm > B_threshold:
-        dB1 *= B_threshold / dB1_norm
-
-    hold2 = "\n---\ndWx_clipped:\n{}\n---\ndWh_clipped:\n{}\n---\ndB1_clipped:\n{}\n\n\n\n-----------".format(dWx, dWh, dB1)
-    #print(hold + hold2)
-
-    return dWx, dWh, dB1, dWo, dB2
+    '''
+    return dWxz, dWhz, dBz, dWxr, dWhr, dBr, dWxh, dWhh, dBh, dWo, dBo
 
 def updateParams(Wx, Wh, B1, Wo, B2, results, batch_size, alpha):
     dWx = np.array([item[0] for item in results])
@@ -122,19 +145,16 @@ def updateParams(Wx, Wh, B1, Wo, B2, results, batch_size, alpha):
 #def processSequence(x, y, weights_biases):
 def processSequence(sequence, weights_biases):
     global amount_correct, amount_tested
-    x, y = sequence
-    #x = (x - np.mean(x, 0)) / np.std(x, 0)
-    x = 2*((x - np.min(x)) / (np.max(x) - np.min(x))) - 1
-    Wx, Wh, B1, Wo, B2 = weights_biases
-    hidden_states_u, hidden_states, Z, A = forwardProp(Wx, Wh, B1, Wo, B2, x)
-    dWx, dWh, dB1, dWo, dB2 = backwardProp(hidden_states_u, hidden_states, A, Z, Wh, Wo, y, x)
+    X, y = sequence
+    foward_results = forwardProp(weights_biases, X)
+    gradients = backwardProp(foward_results, y, X)
     if (np.argmax(A) == y):
         amount_correct += 1
     amount_tested += 1
-    return [dWx, dWh, dB1, dWo, dB2]
+    return gradients
 
 def gradient_descent(X, Y, iterations, alpha):
-    Wx, Wh, B1, Wo, B2 = initParams()
+    Wxz, Whz, Bz, Wxr, Whr, Br, Wxh, Whh, Bh, Wo, Bo = initParams()
     batch_size = 6
     for i in range(iterations):
         print("\n ITERATION: {}\n".format(i + 1))
@@ -143,17 +163,17 @@ def gradient_descent(X, Y, iterations, alpha):
             #print("\n  BATCH: {}\n".format(j + 1))
             x_batch = X[j*batch_size : (j*batch_size)+batch_size]
             y_batch = Y[j*batch_size : (j*batch_size)+batch_size]
-            weights_biases = [Wx, Wh, B1, Wo, B2]
+            weights_biases = [Wxz, Whz, Bz, Wxr, Whr, Br, Wxh, Whh, Bh, Wo, Bo]
 
             
             partial_function = partial(processSequence, weights_biases=weights_biases)
             with Pool(processes=batch_size) as pool:
-                results = pool.map(partial_function, zip(x_batch, y_batch))
-            Wx, Wh, B1, Wo, B2 = updateParams(Wx, Wh, B1, Wo, B2, results, batch_size, alpha)
+                gradients = pool.map(partial_function, zip(x_batch, y_batch))
+            Wxz, Whz, Bz, Wxr, Whr, Br, Wxh, Whh, Bh, Wo, Bo = updateParams(weights_biases, gradients, batch_size, alpha)
             
 
             #processSequence(X[j], Y[j], weights_biases)
-
+            print("\namount_correct:{}".format(amount_correct))
             print("accuracy: {}".format(amount_correct/amount_tested))
     return Wx, Wh, B1, Wo, B2
 
