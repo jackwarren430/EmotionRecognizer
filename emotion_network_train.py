223db0a9c04f5db82144157dbd2ee371b6f56139
jackwarren430 <90930942+jackwarren430@users.noreply.github.com>
2024-02-20 14:34:24 -0500

    changed idk

--- emotion_network_train.py
+++ emotion_network_train.py
@@ -10,21 +10,25 @@ s_length = 90 - 20
 amount_correct = 0
 amount_tested = 1
 
+input_layer_height = 13
+middle_layer_height = 10
+output_layer_height = 8
+
 def initParams():
-    xl = np.sqrt(6 / (10 + 13))
-    hl = np.sqrt(6 / (10 + 10))
-    ol = np.sqrt(6 / (8 + 10))
-    Wxz = np.random.uniform(-xl, xl, size=(10, 13))
-    Whz = np.random.uniform(-hl, hl, size=(10, 10))
-    Bz = np.zeros((10, 1))
-    Wxr = np.random.uniform(-xl, xl, size=(10, 13))
-    Whr = np.random.uniform(-hl, hl, size=(10, 10))
-    Br = np.zeros((10, 1))
-    Wxh = np.random.uniform(-xl, xl, size=(10, 13))
-    Whh = np.random.uniform(-hl, hl, size=(10, 10))
-    Bh = np.zeros((10, 1))
-    Wo = np.random.uniform(-ol, ol, size=(8, 10))
-    Bo = np.zeros((8, 1))
+    xl = np.sqrt(6 / (middle_layer_height + input_layer_height))
+    hl = np.sqrt(6 / (middle_layer_height + middle_layer_height))
+    ol = np.sqrt(6 / (output_layer_height + middle_layer_height))
+    Wxz = np.random.uniform(-xl, xl, size=(middle_layer_height, input_layer_height))
+    Whz = np.random.uniform(-hl, hl, size=(middle_layer_height, middle_layer_height))
+    Bz = np.zeros((middle_layer_height, 1))
+    Wxr = np.random.uniform(-xl, xl, size=(middle_layer_height, input_layer_height))
+    Whr = np.random.uniform(-hl, hl, size=(middle_layer_height, middle_layer_height))
+    Br = np.zeros((middle_layer_height, 1))
+    Wxh = np.random.uniform(-xl, xl, size=(middle_layer_height, input_layer_height))
+    Whh = np.random.uniform(-hl, hl, size=(middle_layer_height, middle_layer_height))
+    Bh = np.zeros((middle_layer_height, 1))
+    Wo = np.random.uniform(-ol, ol, size=(output_layer_height, middle_layer_height))
+    Bo = np.zeros((output_layer_height, 1))
     return [Wxz, Whz, Bz, Wxr, Whr, Br, Wxh, Whh, Bh, Wo, Bo]
 
 def sigmoid(v):
@@ -46,18 +50,18 @@ def softmax(Z):
 
 def forwardProp(weights_biases, X):
     Wxz, Whz, Bz, Wxr, Whr, Br, Wxh, Whh, Bh, Wo, Bo = weights_biases
-    Z_layers = np.zeros((s_length, 10, 1))
-    R_layers = np.zeros((s_length, 10, 1))
-    Hhat_layers = np.zeros((s_length, 10, 1))
-    H_layers = np.zeros((s_length, 10, 1))
-    A_layers = np.zeros((s_length, 8, 1))
-    H = np.zeros((10, 1))
+    Z_layers = np.zeros((s_length, middle_layer_height, 1))
+    R_layers = np.zeros((s_length, middle_layer_height, 1))
+    Hhat_layers = np.zeros((s_length, middle_layer_height, 1))
+    H_layers = np.zeros((s_length, middle_layer_height, 1))
+    A_layers = np.zeros((s_length, output_layer_height, 1))
+    H = np.zeros((middle_layer_height, 1))
     for t in range(s_length):
-        Uz = Wxz @ X[t].reshape(13, 1) + Whz @ H.reshape(10, 1) + Bz.reshape(10, 1)
+        Uz = Wxz @ X[t].reshape(input_layer_height, 1) + Whz @ H.reshape(middle_layer_height, 1) + Bz.reshape(middle_layer_height, 1)
         Z = sigmoid(Uz)
-        Ur = Wxr @ X[t].reshape(13, 1) + Whr @ H.reshape(10, 1) + Br.reshape(10, 1)
+        Ur = Wxr @ X[t].reshape(input_layer_height, 1) + Whr @ H.reshape(middle_layer_height, 1) + Br.reshape(middle_layer_height, 1)
         R = sigmoid(Ur)
-        Uh = Wxh @ X[t].reshape(13, 1) + Whh @ (R * H).reshape(10, 1) + Bh.reshape(10, 1)
+        Uh = Wxh @ X[t].reshape(input_layer_height, 1) + Whh @ (R * H).reshape(middle_layer_height, 1) + Bh.reshape(middle_layer_height, 1)
         Hhat = tanh(Uh)
         Z2 = Wo @ H + Bo
         A = softmax(Z2)
@@ -71,15 +75,15 @@ def forwardProp(weights_biases, X):
 
 
 def backwardProp(forward_results, weights_biases, y, X):
-    Y = np.zeros((8,1))
+    Y = np.zeros((output_layer_height,1))
     Y[int(y-1)][0] = 1
     Z_layers, R_layers, Hhat_layers, H_layers, A_layers = forward_results
     Wxz, Whz, Bz, Wxr, Whr, Br, Wxh, Whh, Bh, Wo, Bo = weights_biases
-    dWxz, dWxr, dWxh = (np.zeros((10, 13)), np.zeros((10, 13)), np.zeros((10, 13)))
-    dWhz, dWhr, dWhh = (np.zeros((10, 10)), np.zeros((10, 10)), np.zeros((10, 10)))
-    dWo = np.zeros((8, 10))
-    dBz, dBr, dBh = (np.zeros((10, 1)), np.zeros((10, 1)), np.zeros((10, 1)))
-    dBo = np.zeros((8, 1))
+    dWxz, dWxr, dWxh = (np.zeros((middle_layer_height, input_layer_height)), np.zeros((middle_layer_height, input_layer_height)), np.zeros((middle_layer_height, input_layer_height)))
+    dWhz, dWhr, dWhh = (np.zeros((middle_layer_height, middle_layer_height)), np.zeros((middle_layer_height, middle_layer_height)), np.zeros((middle_layer_height, middle_layer_height)))
+    dWo = np.zeros((output_layer_height, middle_layer_height))
+    dBz, dBr, dBh = (np.zeros((middle_layer_height, 1)), np.zeros((middle_layer_height, 1)), np.zeros((middle_layer_height, 1)))
+    dBo = np.zeros((output_layer_height, 1))
     for t in reversed(range(s_length)):
         dL_dZ2 = A_layers[t] - Y
         dBo += dL_dZ2
@@ -88,7 +92,7 @@ def backwardProp(forward_results, weights_biases, y, X):
         d0 = dL_dh
 
         Xt = X[t]
-        H_prev = H_layers[t - 1] if t > 0 else np.zeros((10, 1))
+        H_prev = H_layers[t - 1] if t > 0 else np.zeros((middle_layer_height, 1))
         Zt = Z_layers[t]
         Hhat = Hhat_layers[t]
         Rt = R_layers[t]
@@ -101,28 +105,28 @@ def backwardProp(forward_results, weights_biases, y, X):
         d5 = d2 + d4
         d6 = (1 - Zt) * d0
         d7 = d5 * (Zt * (1 - Zt))
-        d8 = d6 * (1 - Hhat**2)
-        #d9 = Wxh.T @ d8
-        d10 = Whh.T @ d8
+        doutput_layer_height = d6 * (1 - Hhat**2)
+        #d9 = Wxh.T @ doutput_layer_height
+        dmiddle_layer_height = Whh.T @ doutput_layer_height
         #d11 = Wxz.T @ d7
         #d12 = Whz.T @ d7
-        #d14 = d10 * Rt
-        d15 = d10 * H_prev
+        #d14 = dmiddle_layer_height * Rt
+        d15 = dmiddle_layer_height * H_prev
         d16 = d15 * (Rt * (1 - Rt))
-        #d13 = Wxr.T @ d16
+        #dinput_layer_height = Wxr.T @ d16
         #d17 = Whr.T @ d16
 
-        dWxr += d16 @ Xt.reshape(1, 13)
-        dWxz += d7 @ Xt.reshape(1, 13)
-        dWxh += d8 @ Xt.reshape(1, 13)
+        dWxr += d16 @ Xt.reshape(1, input_layer_height)
+        dWxz += d7 @ Xt.reshape(1, input_layer_height)
+        dWxh += doutput_layer_height @ Xt.reshape(1, input_layer_height)
 
-        dWhr += d16 @ H_prev.reshape(1, 10)
-        dWhz += d7 @ H_prev.reshape(1, 10)
-        dWhh += d8 @ (H_prev * Rt).reshape(1, 10)
+        dWhr += d16 @ H_prev.reshape(1, middle_layer_height)
+        dWhz += d7 @ H_prev.reshape(1, middle_layer_height)
+        dWhh += doutput_layer_height @ (H_prev * Rt).reshape(1, middle_layer_height)
 
         dBr += d16
         dBz += d7
-        dBh += d8
+        dBh += doutput_layer_height
 
 
     '''
@@ -138,14 +142,14 @@ def backwardProp(forward_results, weights_biases, y, X):
     gradients.insert(2, dBz)
     
 
-    dBz /= 13
-    dBr /= 13
-    Bh /= 13
-    dBo /= 13
+    dBz /= input_layer_height
+    dBr /= input_layer_height
+    Bh /= input_layer_height
+    dBo /= input_layer_height
     '''
 
     gradients = [dWxz, dWhz, dBz, dWxr, dWhr, dBr, dWxh, dWhh, dBh, dWo, dBo]
-    threshold = 3
+    threshold = 1
     for i in range(11):
         norm = np.linalg.norm(gradients[i], ord=2)
         if norm > threshold:
@@ -216,17 +220,21 @@ def gradientDescentLinear(X, Y, iterations, alpha):
         for j in range(X.shape[0]):
             gradients = processSequenceLinear(X[j], Y[j], weights_biases)
             weights_biases = updateParamsLinear(weights_biases, gradients, batch_size, alpha)
-            if (j % 10 == 0):
-                print("accuracy: {}".format(100 * amount_correct/amount_tested))
+            if (j % 30 == 0):
+                print("accuracy: {}".format(10 * amount_correct/amount_tested))
         amount_correct = 0
         amount_tested = 1
-        for i in range(11):
-            print(weights_biases[i])
+        #for i in range(11):
+        #   print(weights_biases[i])
+        shuffled_indices = np.arange(len(x_train))
+        np.random.shuffle(shuffled_indices)
+        X = X[shuffled_indices]
+        Y = Y[shuffled_indices]
     return weights_biases
 
 
 #processSequence([x_train[0], y_train[0]], initParams())
-#Wxz, Whz, Bz, Wxr, Whr, Br, Wxh, Whh, Bh, Wo, Bo = gradientDescent(x_train, y_train, 1000, 0.01)
+#Wxz, Whz, Bz, Wxr, Whr, Br, Wxh, Whh, Bh, Wo, Bo = gradientDescent(x_train, y_train, middle_layer_height00, 0.01)
 Wxz, Whz, Bz, Wxr, Whr, Br, Wxh, Whh, Bh, Wo, Bo = gradientDescentLinear(x_train, y_train, 1000, 0.001)
 
 np.save("./data/weightsBiases/Wxz.npy", Wxz)
